403136742415d838bd6956475b153af736d39794
janiceete <janiceete4@gmail.com>
2022-10-21 18:58:55 +0800

    added update.js

---
+++ update.js
@@ -0,0 +1,15 @@
+const fs = require ('fs')
+
+const update = function (note, oldNote) {
+    const newNote = JSON.parse(oldNote).map(function(n, i){
+
+        if(n.id == note.id){
+            n.tittle = note.tittle;
+            n.body = note.body;
+
+        }
+        return n
+    })
+    fs. writeFileSync('note.txt', JSON.stringify(newNote))
+}
+module.exports - update
